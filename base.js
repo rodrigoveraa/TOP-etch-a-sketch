18abf7ff834f43153c13b84f6498290687c4bc5f
rodrigoveraa <rodrigoeveraa@gmail.com>
2026-09-18 22:11:19 -0300

    Refactor grid creation code
    Moved the code that creates the grid to a new function, createGrid,
    that allows for an arbitrary number of squares per side. This number is
    capped at 100 per the assignment instruction; if it's higher, an
    error message is displayed and no grid is created.
    
    Also created a new function, clearGrid, that removes all elements
    from the main container.

--- base.js
+++ base.js
@@ -1,23 +1,41 @@
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
+const MAX_SQUARES_PER_SIDE = 100;
 
 const mainContainer = document.querySelector(".main-container")
 
-for (let i = 0; i < GRID_HEIGHT; i++) {
-    const newRow = document.createElement("div");
-    newRow.classList.add("grid-row");
-    mainContainer.appendChild(newRow);
+function clearGrid() {
+    mainContainer.replaceChildren();
+}
 
-    for (let j = 0; j < GRID_WIDTH; j++) {
-        const newSquare = document.createElement("div");
-        newSquare.classList.add("grid-square");
-        newSquare.addEventListener("mouseover", function (e) {
-            e.target.classList.add("black-background");
-        });
+function createGrid(squaresPerSide) {
+    if (squaresPerSide > MAX_SQUARES_PER_SIDE) {
+        alert("Too many squares per side!");
+        return;
+    }
 
-        newRow.appendChild(newSquare);
+    for (let i = 0; i < squaresPerSide; i++) {
+        const newRow = document.createElement("div");
+        newRow.classList.add("grid-row");
+        mainContainer.appendChild(newRow);
+
+        for (let j = 0; j < squaresPerSide; j++) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("grid-square");
+            newSquare.addEventListener("mouseover", function (e) {
+                e.target.classList.add("black-background");
+            });
+
+            newRow.appendChild(newSquare);
+            
+        }
         
     }
     
+
 }
+
+clearGrid();
+
+createGrid(50);
+
+
 
